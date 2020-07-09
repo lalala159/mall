f9e132e6af236fec865ae412ef5dd91901fe2958
hecheng <429320998@qq.com>
2020-07-09 11:50:04 +0800

    商品类型查询

--- mall-info/src/main/java/com/mall/info/config/ResourceServerConfig.java
+++ mall-info/src/main/java/com/mall/info/config/ResourceServerConfig.java
@@ -1,4 +1,4 @@
-package com.mall.sys.config;
+package com.mall.info.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
--- mall-info/src/main/java/com/mall/info/dao/sp/MallSplxInfoDao.java
+++ mall-info/src/main/java/com/mall/info/dao/sp/MallSplxInfoDao.java
@@ -1,9 +1,11 @@
 package com.mall.info.dao.sp;
 
 import com.mall.common.domain.sp.MallSplxInfo;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface MallSplxInfoDao {
     int deleteByPrimaryKey(Integer id);
 
